2fb6ba6fd62620adcea5a40186ac234417d48b75
leo-valadao <leonardovaladaooliveira@hotmail.com>
2021-05-24 09:49:50 -0300

    Fibonacci Sequence 1.0.1!

--- Fibonacci Sequence.py
+++ Fibonacci Sequence.py
@@ -8,12 +8,9 @@ Fibonacci = [0, 1]
 num1 = 0
 num2 = 1
 
-if terms == 1:
-    print(Fibonacci[0])
-else:
-    for i in range(0, terms):
-        num3 = num1 + num2
-        Fibonacci.append(num3)
-        num1 = num2
-        num2 = num3
-        print("Fibonacci [", i, "]: ", Fibonacci[i])
+for i in range(0, terms):
+    num3 = num1 + num2
+    Fibonacci.append(num3)
+    num1 = num2
+    num2 = num3
+    print("Fibonacci [", i, "]: ", Fibonacci[i])
